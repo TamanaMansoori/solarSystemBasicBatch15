8dcf8fbce3d5f0a7b33d2fcb74d717d9775e21d8
TamanaMansoori <106633241+TamanaMansoori@users.noreply.github.com>
2023-03-28 18:12:48 -0500

    The basic operational model of the solar system

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello world!");
+
+        solarSystem solar=new solarSystem();
+        solar.sun="1";
+        solar.moon="2";
+        solar.planets="2";
+
+
+    }
+}
